b2690b66ab20ef0a83b250b64a6729f7d27a571b
kvinuk <kevinomuro@gmail.com>
2017-06-20 15:14:42 -0500

    Added edit and update methods for characters, added character_params private method and added edit view

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,20 @@
+class CharactersController < ApplicationController
+	def edit
+		@character = Character.find(params[:id])
+	end
+
+	def update
+		@character = Character.find(params[:id])
+		if @character.update(character_params)
+			redirect_to edit_user_registration_path
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def character_params
+      	params.require(:character).permit(:name, :force_rating, :emotional_strength, :emotional_weakness, :conflict, :morality, :gender, :age, :height, :build, :hair, :eyes, :notable_features, :total_xp, :available_xp, :soak_value, :threshold_wounds, :current_wounds, :threshold_strain, :current_strain, :ranged_defense, :melee_defense, :brawn, :agility, :intellect, :cunning, :will_power, :presence, :credits, :motivations, :species)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :characters, only:[:edit, :update]
 end
