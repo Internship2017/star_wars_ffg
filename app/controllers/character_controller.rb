2099f2ccf4616e8988dd6a9ffb541594f1d065ea
José Antonio Ruiz del Moral Cervantes <joseantoniordlmc@gmail.com>
2017-06-20 15:50:40 -0500

    character_params

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -9,7 +9,7 @@ class CharacterController < ApplicationController
     @character = Character.new(character_params)
 
     if @character.save
-      redirect_to @character, notice: 'Mentor was successfully created.'
+      redirect_to :new_character, notice: 'Mentor was successfully created.'
     else
       render :new
     end
@@ -19,7 +19,12 @@ class CharacterController < ApplicationController
   private
 
   def character_params
-    params.require(:character).permit(:name, :force_rating)
+    params.require(:character).permit(:name, :force_rating, :user, :emotional_strength, :emotional_weakness,
+                                      :conflict, :morality, :gender, :age, :height, :build, :hair, :eyes,
+                                      :notable_features, :total_xp, :available_xp, :soak_value, :threshold_wounds,
+                                      :current_wounds, :threshold_strain, :current_strain, :ranged_defense,
+                                      :melee_defense, :brawn, :agility, :intellect, :cunning, :will_power,
+                                      :presence, :credits, :motivations, :species)
   end
 
 end
