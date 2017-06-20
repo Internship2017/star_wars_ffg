247a3bc516238c3f2e9c156798c30740e63983f2
kvinuk <kevinomuro@gmail.com>
2017-06-20 15:40:57 -0500

    Made the character_params method multiline and erased the empty js, css and helper files

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -15,6 +15,11 @@ class CharactersController < ApplicationController
   private
 
   def character_params
-    params.require(:character).permit(:name, :force_rating, :emotional_strength, :emotional_weakness, :conflict, :morality, :gender, :age, :height, :build, :hair, :eyes, :notable_features, :total_xp, :available_xp, :soak_value, :threshold_wounds, :current_wounds, :threshold_strain, :current_strain, :ranged_defense, :melee_defense, :brawn, :agility, :intellect, :cunning, :will_power, :presence, :credits, :motivations, :species)
+    params.require(:character).permit(:name, :force_rating, :emotional_strength, :emotional_weakness, 
+                                      :conflict, :morality, :gender, :age, :height, :build, :hair, 
+                                      :eyes, :notable_features, :total_xp, :available_xp, :soak_value, 
+                                      :threshold_wounds, :current_wounds, :threshold_strain, :current_strain, 
+                                      :ranged_defense, :melee_defense, :brawn, :agility, :intellect, :cunning, 
+                                      :will_power, :presence, :credits, :motivations, :species)
   end
 end
