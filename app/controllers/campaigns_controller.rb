7dc9ea7dcf8a75a09e452b3139ccf56d836f7c8e
jorgeat <alvarez.atr@gmail.com>
2017-06-21 15:24:06 -0500

    recreated controller and new view

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,4 @@
-class CampaignController < ApplicationController
+class CampaignsController < ApplicationController
   def new
 		@campaign = Campaign.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-	resources :campaigns, {:new, :create}
+  get 'campaigns/new'
+
+  get 'campaigns/create'
+
+	resources :campaigns, only: [:new, :create]
 	
   resources :characters
 
