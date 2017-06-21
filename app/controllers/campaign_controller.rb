18a8bef590bd4e51ac606f168a0e470820709ec6
jorgeat <alvarez.atr@gmail.com>
2017-06-21 14:48:32 -0500

    created campaign controller with new/create

---
+++ app/controllers/campaign_controller.rb
@@ -0,0 +1,20 @@
+class CampaignController < ApplicationController
+  def new
+		@campaign = Campaign.new
+  end
+
+  def create
+		@campaign = Campaign.new(campaign_params)
+
+			if @campaign.save
+					redirect_to @campaign
+				else
+					render 'new'
+			end
+	end
+
+	private
+		def campaign_params
+			params.require(:campaign).permit(:name)
+		end
+end
