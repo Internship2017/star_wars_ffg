503c47f0f093af8c0492b8b061f649f779bcf02e
José Antonio Ruiz del Moral Cervantes <joseantoniordlmc@gmail.com>
2017-06-21 15:47:21 -0500

    Added Campaign:Index

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,20 +1,33 @@
 class CampaignsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_campaign, only: [:edit, :update, :show, :destroy]
+
   def new
-		@campaign = Campaign.new
+    @campaign = Campaign.new
   end
 
   def create
-		@campaign = Campaign.new(campaign_params)
+    @campaign = Campaign.new(campaign_params)
 
-			if @campaign.save
-					redirect_to @campaign
-				else
-					render 'new'
-			end
-	end
+    if @campaign.save
+      redirect_to @campaign
+    else
+      render 'new'
+    end
+  end
 
-	private
-		def campaign_params
-			params.require(:campaign).permit(:name)
-		end
+  def index
+    @characters = current_user.characters
+  end
+
+  def show
+    @campaign = current_user.characters.find_by(id: params[:id])
+  end
+
+  private
+
+
+  def campaign_params
+    params.require(:campaign).permit(:name)
+  end
 end
