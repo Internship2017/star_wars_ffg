81133e6a49dab028ba16d8e9d3e34562134542ba
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-06-21 17:23:03 -0500

    Fix rubocop's offenses

--- spec/factories/character_weapons.rb
+++ spec/factories/character_weapons.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
-	factory :character_weapon do
-		character
-		weapon
-		damage 1
-		crit 1
-		special { FFaker::Lorem.paragraph }
-	end
+  factory :character_weapon do
+    character
+    weapon
+    damage 1
+    crit 1
+    special { FFaker::Lorem.paragraph }
+  end
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Weapon, type: :model do
   it { should validate_presence_of(:skill) }
   it { should validate_presence_of(:range) }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-	it { should validate_numericality_of(:encum).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
-	it { should validate_numericality_of(:hp).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
-	it { should validate_numericality_of(:rarity).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
-	it { should have_many(:characters)}
+  it { should validate_numericality_of(:encum).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
+  it { should validate_numericality_of(:hp).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
+  it { should validate_numericality_of(:rarity).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(99) }
+  it { should have_many(:characters)}
 end
