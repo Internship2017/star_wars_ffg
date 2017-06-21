349132903ebba3c1a9c8a852fdc12c99343fab78
Israelhz <israel.leal25@gmail.com>
2017-06-21 16:09:51 -0500

    Adds dependent destroy between weapons and characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :character_weapons
+  has_many :character_weapons, dependent: :destroy
   has_many :weapons, through: :character_weapons
   
   validates :name, :species, :motivations, :emotional_strength, :emotional_weakness, 
--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,4 +1,4 @@
 class Weapon < ApplicationRecord
-	has_many :character_weapons
+	has_many :character_weapons, dependent: :destroy
 	has_many :characters, through: :character_weapons
 end
