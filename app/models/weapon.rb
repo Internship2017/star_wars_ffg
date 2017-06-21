b532e358521194d5a6a8e9a822a1b4f199ed4180
Israelhz <israel.leal25@gmail.com>
2017-06-21 16:19:04 -0500

    Adds model validation for weapon and character_weapon

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,4 +1,10 @@
 class Weapon < ApplicationRecord
 	has_many :character_weapons, dependent: :destroy
 	has_many :characters, through: :character_weapons
+
+	validates :name, :skill, :range, presence: true
+
+	validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+	validates :encum, :hp, :rarity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 99 }
 end
