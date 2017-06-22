bc492dd1e2635c367fb2c3e2bf1e874069a19256
Ana Paola Treviño <anapaolatrevino@Anas-MacBook-Air.local>
2017-06-22 15:21:38 -0500

    Fix view Skill

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def select_type(skill_type)
+    @character.skills.order(:name).select{ |type_skill| type_skill.type_of_skill == skill_type }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :characters
   resources :careers
 
-  resources :skills
-
   devise_for :users
 
 end
