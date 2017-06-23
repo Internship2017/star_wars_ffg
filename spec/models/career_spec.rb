ae3e32e06a2d8c714f2ba5441b6ddd05c612cb8e
kvinuk <kevinomuro@gmail.com>
2017-06-23 11:30:26 -0500

    Changed limit in career_skills on career model from 6 to 1..10

--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -16,13 +16,28 @@ RSpec.describe Career, type: :model do
   it { should validate_presence_of(:career_skills) }
   it { should validate_numericality_of(:source_page).only_integer }
   it { should have_many(:characters) }
-  it "validates length of career_skills is 6" do
-    career = FactoryGirl.create(:career, career_skills: %w[Warrior Guardian Healer Sniper Tank Paladin])
+  it "validates length of career_skills as 6 is valid" do
+    career = FactoryGirl.build(:career, career_skills: %w[Warrior Guardian Healer Sniper Tank Paladin])
     expect(career).to be_valid
   end
 
-  it "validates length of career_skills is not 6" do
-    career = FactoryGirl.build(:career, career_skills: %w[Warrior Guardian])
+  it "validates length of career_skills as 1 is valid" do
+    career = FactoryGirl.build(:career, career_skills: %w[Healer])
+    expect(career).to be_valid
+  end
+
+  it "validates length of career_skills as 10 is valid" do
+    career = FactoryGirl.build(:career, career_skills: %w[Warrior Guardian Healer Sniper Tank Paladin Healer Sniper Tank Paladin])
+    expect(career).to be_valid
+  end
+
+  it "validates length of career_skills as 0 is not valid" do
+    career = FactoryGirl.build(:career, career_skills: [])
+    expect(career).not_to be_valid
+  end
+
+  it "validates length of career_skills as 11 is not valid" do
+    career = FactoryGirl.build(:career, career_skills: %w[Warrior Guardian Healer Sniper Tank Paladin Healer Sniper Tank Paladin Offtank])
     expect(career).not_to be_valid
   end
 
