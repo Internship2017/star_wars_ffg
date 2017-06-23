9a6a021c890ce52b21ee9468c0d9e60515cf3f00
Ana Paola Treviño <anapaolatrevino@Anas-MacBook-Air.local>
2017-06-23 14:59:49 -0500

    Fixed conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-
   resources :campaigns, except: [:delete]
   resources :characters
   resources :careers do
     post :upload, on: :collection
   end
+
+  
 
   devise_for :users
 
