0863468f65d2e570aeab2c55037e22548f0697a9
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-06-24 16:10:31 -0500

    Adds default values to crit and damage migration

--- db/migrate/20170622185753_add_columns_to_weapon.rb
+++ db/migrate/20170622185753_add_columns_to_weapon.rb
@@ -1,6 +1,6 @@
 class AddColumnsToWeapon < ActiveRecord::Migration[5.1]
   def change
-  	add_column :weapons, :default_damage, :integer
-  	add_column :weapons, :default_crit, :integer
+    add_column :weapons, :default_damage, :integer, default: 1
+    add_column :weapons, :default_crit, :integer, default: 1
   end
 end
