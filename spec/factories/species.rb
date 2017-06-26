1bdb52f2326a407e81e6ef67a4f2924f6ee02ab5
José Antonio Ruiz del Moral Cervantes <joseantoniordlmc@gmail.com>
2017-06-26 09:49:28 -0500

    Fixed identation on species, factory, model, and validations

--- spec/factories/species.rb
+++ spec/factories/species.rb
@@ -1,20 +1,20 @@
 FactoryGirl.define do
-	factory :species do
-		name { FFaker::Lorem.paragraph }
-		description { FFaker::Lorem.paragraph }
-		source_page { (rand * 100).to_i }
-		source_book { "Force and Destiny Core Rulebook" }
-		brawn { 1 }
-		agility { 1 }
-		intellect { 1 }
-		cunning { 1 }
-		willpower { 1 }
-		presence { 1 }
-		wound_threshold { 50 }
-		strain_threshold { 50 }
-		experience { 50 }
-		skill_name { FFaker::Name.name }
-		skill_start_rank { 1 }
-		skill_limit_rank { 1 }
-	end
+  factory :species do
+    name {FFaker::Lorem.paragraph}
+    description {FFaker::Lorem.paragraph}
+    source_page {(rand * 100).to_i}
+    source_book {"Force and Destiny Core Rulebook"}
+    brawn {1}
+    agility {1}
+    intellect {1}
+    cunning {1}
+    willpower {1}
+    presence {1}
+    wound_threshold {50}
+    strain_threshold {50}
+    experience {50}
+    skill_name {FFaker::Name.name}
+    skill_start_rank {1}
+    skill_limit_rank {1}
+  end
 end
