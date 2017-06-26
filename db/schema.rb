a209f3fdb5f24fd1fd0e2919bc062c8cf6c0498b
Jorge Alvarez <JorgeATR@users.noreply.github.com>
2017-06-26 15:03:29 -0500

    Species model (#56)
    * created species model
    
    * added tests, factory and model validations for Species
    
    * Fixed identation on species, factory, model, and validations
    
    * Fixed requested changes
    
    * Fix on specie factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623211731) do
+ActiveRecord::Schema.define(version: 20170623204626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,27 @@ ActiveRecord::Schema.define(version: 20170623211731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["career_id"], name: "index_specializations_on_career_id"
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "source_page"
+    t.string "source_book"
+    t.integer "brawn"
+    t.integer "agility"
+    t.integer "intellect"
+    t.integer "cunning"
+    t.integer "willpower"
+    t.integer "presence"
+    t.integer "wound_threshold"
+    t.integer "strain_threshold"
+    t.integer "experience"
+    t.string "skill_name"
+    t.integer "skill_start_rank"
+    t.integer "skill_limit_rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "talents", force: :cascade do |t|
