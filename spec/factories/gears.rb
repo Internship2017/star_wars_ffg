c46b1168af81e89968ce8642c80de74df1076c93
Israelhz <israel.leal25@gmail.com>
2017-06-28 14:43:07 -0500

    Adds character_gear and gear factory

---
+++ spec/factories/gears.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :gear do
+    name { ["Comlink (handheld)", "Comlink (long range)", "Holo-messenger", "Pioneer Long-Range Hypertransceiver", "Whistler Encryption Module", "Remote DVI Activator (Short Range)"].sample }
+    encum 1
+    price { rand(200...210) }
+    rarity { rand(1...7) }
+    type "Communications"
+  end
+end
