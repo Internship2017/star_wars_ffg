e566e4f0c1315cd1b5cb5c803d0d80506cb3baf0
Israelhz <israel.leal25@gmail.com>
2017-06-28 16:47:08 -0500

    Adds seed for Gear

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,2 +1,10 @@
 class Gear < ApplicationRecord
+	has_many :character_gears, dependent: :destroy
+  has_many :characters, through: :character_gears
+
+	def self.parse_gears(json_gear_directory = 'lib/data/Gear.json')
+    file = File.read(Rails.root.join(json_gear_directory))
+    data_hash = JSON.parse(file)
+    data_hash["Gears"]["Gear"]
+  end
 end
