b45ee22861c8c4ce148750f3cdecc9f36b82f7c7
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-06-29 15:26:09 -0500

    Removes current character and weapon methods

--- app/controllers/character_weapons_controller.rb
+++ app/controllers/character_weapons_controller.rb
@@ -14,14 +14,16 @@ class CharacterWeaponsController < ApplicationController
 
   def new
     @weapons = Weapon.order_by_rarity.order_by_price
-    @character = current_character
+    @character = Character.find(params[:character_id])
   end
 
   def create
-    @character_weapon = CharacterWeapon.new(character: current_character, weapon: current_weapon, damage: current_weapon.default_damage, crit: current_weapon.default_crit)
+    character = Character.find(params[:character_id])
+    weapon = Weapon.find(params[:weapon_id])
+    @character_weapon = CharacterWeapon.new(character: character, weapon: weapon, damage: weapon.default_damage, crit: weapon.default_crit)
 
     if @character_weapon.save
-      redirect_to current_character, notice: "Weapon #{@character_weapon.weapon.name} was successfully created."
+      redirect_to @character_weapon.character, notice: "Weapon #{@character_weapon.weapon.name} was successfully created."
     else
       render :new, flash: { danger: @character_weapon.errors }
     end
@@ -47,14 +49,6 @@ class CharacterWeaponsController < ApplicationController
 
   def verify_character_weapon
     redirect_to edit_user_registration_path, flash: { danger: "YOU SHALL NOT SEE OTHER'S CHARACTERS" } unless @character_weapon
-  end
-
-  def current_character
-    @current_character ||= Character.find(params[:character_id])
-  end
-
-  def current_weapon
-    @current_weapon ||= Weapon.find(params[:weapon_id])
   end
 
   def character_weapon_params
