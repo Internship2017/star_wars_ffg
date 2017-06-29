0bb4f3189642469c37f7bc638d4f2735a945b1f6
José Antonio Ruiz del Moral Cervantes <joseantoniordlmc@gmail.com>
2017-06-29 16:50:39 -0500

    Fix on SpeciesUploader with reek

--- app/services/species_uploader.rb
+++ app/services/species_uploader.rb
@@ -7,7 +7,7 @@ class SpeciesUploader
   def run
     JsonSpecies.to_a(@json_file).each do |json_specie|
       json_specie_attrs = json_specie.attributes
-      Species.with_name(json_specie.name).first_or_create(json_specie_attrs).update(json_specie_attrs)
+      Species.with_name(json_specie_attrs.name).first_or_create(json_specie_attrs).update(json_specie_attrs)
     end
   end
 end
