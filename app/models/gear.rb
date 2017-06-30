6a325cb95ebb0e590b71dcba92f3895fc3361840
Israelhz <israel.leal25@gmail.com>
2017-06-30 15:46:15 -0500

    Update gear.rb

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,5 +1,5 @@
 class Gear < ApplicationRecord
-	has_many :character_gears, dependent: :destroy
+  has_many :character_gears, dependent: :destroy
   has_many :characters, through: :character_gears
   validates :name, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -8,7 +8,7 @@ class Gear < ApplicationRecord
   scope :order_by_rarity, -> { order(rarity: :desc) }
   scope :order_by_price, -> { order(price: :desc) }
 
-	def self.parse_gears(json_gear_directory = 'lib/data/Gear.json')
+  def self.parse_gears(json_gear_directory = 'lib/data/Gear.json')
     file = File.read(Rails.root.join(json_gear_directory))
     data_hash = JSON.parse(file)
     data_hash["Gears"]["Gear"]
